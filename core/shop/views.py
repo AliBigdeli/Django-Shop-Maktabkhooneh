2107b5997b89520eca9133b1d2f4deebd1d1a9c3
TheAliBigdeli <bigdeli.ali3@gmail.com>
2024-01-11 14:51:04 +0330

    added filters for django list view

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,5 +1,4 @@
 from typing import Any
-from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import (
     TemplateView,
@@ -7,13 +6,16 @@ from django.views.generic import (
     DetailView,
 )
 from .models import ProductModel, ProductStatusType,ProductCategoryModel
+from django.core.exceptions import FieldError
 # Create your views here.
 
 
 class ShopProductGridView(ListView):
     template_name = "shop/product-grid.html"
-
     paginate_by = 9
+    
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get('page_size',self.paginate_by)
 
     def get_queryset(self):
         queryset = ProductModel.objects.filter(
@@ -22,6 +24,15 @@ class ShopProductGridView(ListView):
             queryset = queryset.filter(title__icontains=search_q)
         if category_id:=self.request.GET.get("category_id"):
             queryset = queryset.filter(category__id=category_id)
+        if min_price:= self.request.GET.get("min_price"):
+            queryset = queryset.filter(price__gte=min_price)
+        if max_price:= self.request.GET.get("max_price"):
+            queryset = queryset.filter(price__lte=max_price)
+        if order_by:= self.request.GET.get("order_by"):
+            try:
+                queryset = queryset.order_by(order_by)
+            except FieldError:
+                pass
         return queryset
 
     def get_context_data(self, **kwargs):
