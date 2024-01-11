cc87e761018c13f3fab11a91cf1bb9343284340f
TheAliBigdeli <bigdeli.ali3@gmail.com>
2024-01-11 14:08:14 +0330

    added regex for url

--- core/shop/urls.py
+++ core/shop/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 app_name = "shop"
 
 urlpatterns = [
     path("product/grid/",views.ShopProductGridView.as_view(),name="product-grid"),
-    path("product/<slug:slug>/detail/",views.ShopProductDetailView.as_view(),name="product-detail"),
+    re_path(r"product/(?P<slug>[-\w]+)/detail/",views.ShopProductDetailView.as_view(),name="product-detail"),
 ]
 
