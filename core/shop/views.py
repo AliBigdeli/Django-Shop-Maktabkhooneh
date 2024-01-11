12b6dcb8640db55913a1154266cc2e8c656f61d7
TheAliBigdeli <bigdeli.ali3@gmail.com>
2024-01-11 14:26:45 +0330

    added product category and q filter

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,23 +1,33 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import (
     TemplateView,
     ListView,
     DetailView,
 )
-from .models import ProductModel, ProductStatusType
+from .models import ProductModel, ProductStatusType,ProductCategoryModel
 # Create your views here.
 
 
 class ShopProductGridView(ListView):
     template_name = "shop/product-grid.html"
-    queryset = ProductModel.objects.filter(
-        status=ProductStatusType.publish.value)
+
     paginate_by = 9
 
+    def get_queryset(self):
+        queryset = ProductModel.objects.filter(
+            status=ProductStatusType.publish.value)
+        if search_q:=self.request.GET.get("q"):
+            queryset = queryset.filter(title__icontains=search_q)
+        if category_id:=self.request.GET.get("category_id"):
+            queryset = queryset.filter(category__id=category_id)
+        return queryset
+
     def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context["total_items"] = self.get_queryset().count()
+        context["categories"] = ProductCategoryModel.objects.all()
         return context
 
 
