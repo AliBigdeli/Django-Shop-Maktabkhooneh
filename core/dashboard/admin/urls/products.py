6b20caf8c3fd286b356ad9d383c377eeefe75f68
TheAliBigdeli <bigdeli.ali3@gmail.com>
2024-01-14 22:50:51 +0330

    added product management

--- core/dashboard/admin/urls/products.py
+++ core/dashboard/admin/urls/products.py
@@ -4,5 +4,7 @@ from .. import views
 
 urlpatterns = [
     path("product/list/",views.AdminProductListView.as_view(),name="product-list"),
-    path("product/<int:pk>/edit/",views.AdminProductEditView.as_view(),name="product-edit")
+    path("product/create/",views.AdminProductCreateView.as_view(),name="product-create"),
+    path("product/<int:pk>/edit/",views.AdminProductEditView.as_view(),name="product-edit"),
+    path("product/<int:pk>/delete/",views.AdminProductDeleteView.as_view(),name="product-delete")
 ]
--- core/dashboard/admin/views/products.py
+++ core/dashboard/admin/views/products.py
@@ -1,4 +1,13 @@
-from django.views.generic import View, TemplateView,UpdateView,ListView
+from django.forms.forms import BaseForm
+from django.http.response import HttpResponse
+from django.views.generic import (
+    View,
+    TemplateView,
+    UpdateView,
+    ListView,
+    DeleteView,
+    CreateView
+)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from dashboard.permissions import HasAdminAccessPermission
 from django.contrib.auth import views as auth_views
@@ -8,27 +17,28 @@ from django.urls import reverse_lazy
 from accounts.models import Profile
 from django.shortcuts import redirect
 from django.contrib import messages
-from shop.models import ProductModel,ProductCategoryModel,ProductStatusType
+from shop.models import ProductModel, ProductCategoryModel, ProductStatusType
 from django.core.exceptions import FieldError
 
-class AdminProductListView(LoginRequiredMixin, HasAdminAccessPermission,ListView):
+
+class AdminProductListView(LoginRequiredMixin, HasAdminAccessPermission, ListView):
     template_name = "dashboard/admin/products/product-list.html"
     paginate_by = 10
-    
+
     def get_paginate_by(self, queryset):
-        return self.request.GET.get('page_size',self.paginate_by)
+        return self.request.GET.get('page_size', self.paginate_by)
 
     def get_queryset(self):
         queryset = ProductModel.objects.all()
-        if search_q:=self.request.GET.get("q"):
+        if search_q := self.request.GET.get("q"):
             queryset = queryset.filter(title__icontains=search_q)
-        if category_id:=self.request.GET.get("category_id"):
+        if category_id := self.request.GET.get("category_id"):
             queryset = queryset.filter(category__id=category_id)
-        if min_price:= self.request.GET.get("min_price"):
+        if min_price := self.request.GET.get("min_price"):
             queryset = queryset.filter(price__gte=min_price)
-        if max_price:= self.request.GET.get("max_price"):
+        if max_price := self.request.GET.get("max_price"):
             queryset = queryset.filter(price__lte=max_price)
-        if order_by:= self.request.GET.get("order_by"):
+        if order_by := self.request.GET.get("order_by"):
             try:
                 queryset = queryset.order_by(order_by)
             except FieldError:
@@ -38,14 +48,38 @@ class AdminProductListView(LoginRequiredMixin, HasAdminAccessPermission,ListView
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["total_items"] = self.get_queryset().count()
-        context["categories"] = ProductCategoryModel.objects.all()        
+        context["categories"] = ProductCategoryModel.objects.all()
         return context
-    
-class AdminProductEditView(LoginRequiredMixin, HasAdminAccessPermission,SuccessMessageMixin,UpdateView):
+
+
+class AdminProductCreateView(LoginRequiredMixin, HasAdminAccessPermission, SuccessMessageMixin, CreateView):
+    template_name = "dashboard/admin/products/product-create.html"
+    queryset = ProductModel.objects.all()
+    form_class = ProductForm
+    success_message = "ایجاد محصول با موفقیت انجام شد"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super().form_valid(form)
+        return redirect(reverse_lazy("dashboard:admin:product-edit", kwargs={"pk": form.instance.pk}))
+        
+
+    def get_success_url(self):
+        return reverse_lazy("dashboard:admin:product-list")
+
+
+class AdminProductEditView(LoginRequiredMixin, HasAdminAccessPermission, SuccessMessageMixin, UpdateView):
     template_name = "dashboard/admin/products/product-edit.html"
     queryset = ProductModel.objects.all()
     form_class = ProductForm
     success_message = "ویرایش محصول با موفقیت انجام شد"
-    
+
     def get_success_url(self):
-        return reverse_lazy("dashboard:admin:product-edit",kwargs={"pk":self.get_object().pk})
+        return reverse_lazy("dashboard:admin:product-edit", kwargs={"pk": self.get_object().pk})
+
+
+class AdminProductDeleteView(LoginRequiredMixin, HasAdminAccessPermission, SuccessMessageMixin, DeleteView):
+    template_name = "dashboard/admin/products/product-delete.html"
+    queryset = ProductModel.objects.all()
+    success_url = reverse_lazy("dashboard:admin:product-list")
+    success_message = "حذف محصول با موفقیت انجام شد"
