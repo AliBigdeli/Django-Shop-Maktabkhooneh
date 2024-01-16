128636d81c0e53cfa07b75a9ff4d526471e3a0cf
TheAliBigdeli <bigdeli.ali3@gmail.com>
2024-01-16 13:34:15 +0330

    added form and template for checkout

--- core/order/views.py
+++ core/order/views.py
@@ -3,8 +3,15 @@ from django.views.generic import (
 )
 from django.contrib.auth.mixins import LoginRequiredMixin
 from order.permissions import HasCustomerAccessPermission
+from order.models import UserAddressModel
 # Create your views here.
 
 
 class OrderCheckOutView(LoginRequiredMixin, HasCustomerAccessPermission,TemplateView):
     template_name = "order/checkout.html"
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["addresses"] = UserAddressModel.objects.filter(user=self.request.user)
+        return context
